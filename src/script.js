1601f02876ea88f7ce7ffac11e02b1e7001adecb
Saim <saimhurhasan@cedcoss.com>
2022-02-05 10:53:28 +0530

    7 commit

--- src/script.js
+++ src/script.js
@@ -11,3 +11,12 @@ b =a;
 var a=9;
 var myFirstName ="saim";
 var myLastName ="hasan";
+// Only change code below this line
+var a =5;
+var b =10;
+var c ="I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
