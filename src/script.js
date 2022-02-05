afd802f99dedca2a70a1a923166fedd96c8248d1
Saim <saimhurhasan@cedcoss.com>
2022-02-05 15:19:31 +0530

    71 commit

--- src/script.js
+++ src/script.js
@@ -422,3 +422,20 @@ function nextInLine(arr, item) {
   }
   
   testLogicalOr(15);
+  function testElse(val) {
+    let result = "";
+    // Only change code below this line
+  
+    if (val > 5) {
+      result = "Bigger than 5";
+    }
+  
+    else {
+      result = "5 or Smaller";
+    }
+  
+    // Only change code above this line
+    return result;
+  }
+  
+  testElse(4);
