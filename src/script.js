b19326f4dacef1847710420f52c90b0519a17d1d
Saim <saimhurhasan@cedcoss.com>
2022-02-05 15:52:35 +0530

    76 commit

--- src/script.js
+++ src/script.js
@@ -537,3 +537,21 @@ function golfScore(par, strokes) {
 }
 
 golfScore(5, 4);
+function caseInSwitch(val) {
+  let answer = " ";
+  // Only change code below this line
+  switch(val) {
+  case 1:
+    answer="alpha";
+    break;
+  case 2:
+    answer="beta";
+    break;
+    case 3:
+    answer="gamma";
+    break;
+    case 4:
+    answer="delta";
+    break;
+}
+}
