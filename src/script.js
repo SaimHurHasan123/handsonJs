e1fddcd6b8321c876579206190997d78d2ea1725
Saim <saimhurhasan@cedcoss.com>
2022-02-05 14:36:47 +0530

    61 commit

--- src/script.js
+++ src/script.js
@@ -310,3 +310,11 @@ function nextInLine(arr, item) {
   }
   
   testEqual(10);
+  function testStrict(val) {
+    if (val===7) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  testStrict(10);
