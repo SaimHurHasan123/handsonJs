05687da77d906aa7b860d5f92c1b01a8771f0529
Saim <saimhurhasan@cedcoss.com>
2022-02-05 13:37:04 +0530

    53 commit

--- src/script.js
+++ src/script.js
@@ -215,3 +215,13 @@ function fun2() {
   }
   console.log(output);
 }
+function myLocalScope() {
+    // Only change code below this line
+  const myVar=5;
+    console.log('inside myLocalScope', myVar);
+  }
+  myLocalScope();
+  console.log(myVar);
+  // Run and check the console
+  // myVar is not defined outside of myLocalScope
+  console.log('outside myLocalScope', myVar);
