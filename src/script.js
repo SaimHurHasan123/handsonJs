8c257b7db5be80e45206e17be67bd87809eb85cc
Saim <saimhurhasan@cedcoss.com>
2022-02-05 14:03:28 +0530

    59 commit

--- src/script.js
+++ src/script.js
@@ -289,3 +289,16 @@ function nextInLine(arr, item) {
   
     // Only change code above this line
   }
+  function trueOrFalse(wasThatTrue) {
+    // Only change code below this line
+  
+   if (wasThatTrue) {
+      return "Yes, that was true";
+    }
+    return "No, that was false";
+  }
+    // Only change code above this line
+  
+  
+  trueOrFalse(true);
+  trueOrFalse(false);
