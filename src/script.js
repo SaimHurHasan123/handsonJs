0e7679b9a4781494bd5212e549dcfb1b3dd8c6ec
Saim <saimhurhasan@cedcoss.com>
2022-02-05 14:49:53 +0530

    66 commit

--- src/script.js
+++ src/script.js
@@ -355,3 +355,17 @@ function nextInLine(arr, item) {
   }
   
   testGreaterThan(10);
+  function testGreaterOrEqual(val) {
+    if (val>=20) {  
+      return "20 or Over";
+    }
+  
+    if (val>=10) {  
+      return "10 or Over";
+    }
+  
+    return "Less than 10";
+  }
+  
+  testGreaterOrEqual(10);
+  
